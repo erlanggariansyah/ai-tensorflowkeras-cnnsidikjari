4ddbae18e2520cd383257976efa31d4b5f078449
anggaifg <erlanggariansyah0@gmail.com>
2023-06-24 22:31:31 +0700

    Data Preprocessing: Resize to 24x24 pixel

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ batch_size = 32
 # Fungsi untuk memuat gambar dengan ekstensi .tif dan mengubahnya menjadi array numpy
 def load_tiff_image(image_path, target_size):
     image = tifffile.imread(image_path)
+    image = tf.expand_dims(image, axis=-1)  # Menambahkan dimensi saluran pada gambar
     image_resized = tf.image.resize(image, target_size)
     image_array = tf.keras.preprocessing.image.img_to_array(image_resized)
     return image_array
@@ -22,18 +23,24 @@ def load_tiff_image(image_path, target_size):
 # Membuat objek ImageDataGenerator untuk melakukan augmentasi atau pemrosesan tambahan
 datagen = ImageDataGenerator(rescale=1./255)  # Contoh augmentasi: normalisasi skala piksel
 
-# Memuat data pelatihan dari folder 'dataset1' menggunakan objek ImageDataGenerator
+# Mendapatkan daftar nama kelas
+class_names = sorted(os.listdir(dataset_path))
+
+# Memuat data pelatihan dari folder 'dataset' menggunakan objek ImageDataGenerator
 train_data = datagen.flow_from_directory(
     dataset_path,
-    target_size=(128, 128),  # Ubah sesuai dengan ukuran yang diinginkan
-    batch_size=batch_size
+    target_size=target_size,
+    batch_size=batch_size,
+    classes=class_names,
+    shuffle=True
 )
 
 # Lakukan pra-pemrosesan dan ubah ukuran gambar menjadi 24x24 pixel
 resized_train_data = []
 for images, labels in train_data:
     resized_images = []
-    for image_path in images:
+    for i in range(len(images)):
+        image_path = train_data.filepaths[i]
         tiff_image = load_tiff_image(image_path, target_size)
         resized_images.append(tiff_image)
     resized_images = tf.stack(resized_images)
@@ -41,5 +48,3 @@ for images, labels in train_data:
     
     if len(resized_train_data) >= len(train_data):
         break
-
-# Pastikan bahwa direktori 'dataset1' berada di direktori root proyek Anda sebelum menjalankan main.py.
